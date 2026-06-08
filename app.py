7aa6284272db805148e64d624fcfd2fcac6bc666
pranay  somnathe <pranaysomnathe69@gmail.com>
2026-06-08 22:15:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ elif menu == "Add Medicine":
        INSERT INTO medicines(name, quantity, price, expiry_date, location)
        VALUES (?, ?, ?, ?, ?)
        """, (name, quantity, price, str(expiry), location))
-        st.success("Medicine Added Successfully")
+       st.success("Medicine Added Successfully")
 
 elif menu == "View Medicines":
 
