9b1ee708b6f5af5c7f2f808f6cfe902c917e4d9a
pranay  somnathe <pranaysomnathe69@gmail.com>
2026-06-08 22:09:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ elif menu == "Add Medicine":
        INSERT INTO medicines(name, quantity, price, expiry_date, location)
        VALUES (?, ?, ?, ?, ?)
        """, (name, quantity, price, str(expiry), location))
-        )
+        
 
         conn.commit()
 
